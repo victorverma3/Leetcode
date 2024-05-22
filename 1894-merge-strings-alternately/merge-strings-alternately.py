3df891fb85174adc6618d5b89ad1c382a8dc2913
Victor Verma <vpverm@bu.edu>
2024-05-22 00:45:26 -0400

    Time: 26 ms (10.13%) | Memory: 11.8 MB (29.45%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,15 +5,12 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        l1 = len(word1)
-        l2 = len(word2)
-        string = ""
-        if l1 < l2:
-            for i in range(l1):
-                string = string + word1[i] + word2[i]
-            string += word2[l1:]
-        else:
-            for i in range(l2):
-                string = string + word1[i] + word2[i]
-            string += word1[l2:]
-        return string
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+        return ''.join(result)
