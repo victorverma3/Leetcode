db3421715e636a8c8daabf8aa17c8fe3e95adfec
Victor Verma <vpverm@bu.edu>
2024-05-23 15:11:48 -0400

    Time: 111 ms (87.11%) | Memory: 12.4 MB (16.49%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -11,14 +11,9 @@ class Solution(object):
                 return count >= n
             else:
                 return count + 1>= n
-        if flowerbed[0] == 0 and flowerbed[1] == 0:
-            flowerbed[0] = 1
-            count += 1
-        for i in range(1, len(flowerbed) - 1):
-            if flowerbed[i-1:i+2] == [0, 0, 0]:
+
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i+1] == 0):
                 flowerbed[i] = 1
                 count += 1
-        if flowerbed[-2] == 0 and flowerbed[-1] == 0:
-            flowerbed[-1] = 1
-            count += 1
         return count >= n
