2c085edbe5b37a63dbe113b8352843b585b10195
Victor Verma <vpverm@bu.edu>
2024-06-06 00:11:28 -0400

    Time: 81 ms (28.69%) | Memory: 15 MB (75.03%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        frequencies = defaultdict(int)
+        for i in nums:
+            frequencies[i] += 1
+        freq = [(key, frequencies[key]) for key in frequencies.keys()]
+
+        def mergesort(array):
+            if len(array) < 2:
+                return array[:]
+            mid = len(array) // 2
+            left = mergesort(array[:mid])
+            right = mergesort(array[mid:])
+            return merge(left, mid, right)
+
+        def merge(left, mid, right):
+            helper = []
+            i = 0
+            j = 0
+
+            while i < mid and j < len(right):
+                if left[i][1] > right[j][1]:
+                    helper.append(left[i])
+                    i += 1
+                else:
+                    helper.append(right[j])
+                    j += 1
+            
+            while i < mid:
+                helper.append(left[i])
+                i += 1
+            while j < len(right):
+                helper.append(right[j])
+                j += 1
+            
+            return helper
+
+        sorted_freq = mergesort(freq)
+        most_freq = [freq[0] for freq in sorted_freq]
+        return most_freq[:k]
