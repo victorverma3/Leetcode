780c8bb34c344756328fe33eb2c51b6c80a25d80
Victor Verma <vpverm@bu.edu>
2024-06-25 19:36:02 -0400

    Time: 27 ms (72.24%) | Memory: 12.3 MB (88.05%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) == 0:
+            return True
+        digits = set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left] in digits or s[right] in digits:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            else:
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+        return True
