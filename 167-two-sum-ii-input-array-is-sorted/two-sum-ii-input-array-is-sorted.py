e455d7b6fc1f5840a4e88845ed4c268d0f7e01f2
Victor Verma <vpverm@bu.edu>
2024-06-25 20:07:54 -0400

    Time: 94 ms (27.02%) | Memory: 12.6 MB (33.89%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -8,9 +8,9 @@ class Solution(object):
         left = 0
         right = len(numbers) - 1
         while left < right:
-            if numbers[left] + numbers[right] == target:
-                return [left + 1, right + 1]
-            elif numbers[left] + numbers[right] < target:
+            if numbers[left] + numbers[right] < target:
                 left += 1
-            else:
+            elif numbers[left] + numbers[right] > target:
                 right -= 1
+            else:
+                return [left + 1, right + 1]
