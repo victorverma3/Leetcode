1a7957ff6146ea6bc53704f4d6445faa1359bf21
Victor Verma <vpverm@bu.edu>
2024-06-28 16:03:43 -0400

    Time: 184 ms (23.51%) | Memory: 12.7 MB (79.35%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return -1
