3d39850bea246f31e9afd83a1ea59305c8e32c4c
Victor Verma <vpverm@bu.edu>
2024-06-30 01:26:18 -0400

    Time: 19 ms (30.36%) | Memory: 11.4 MB (96.15%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        values = []
+        if root is None:
+            return values
+
+        def bfs(root):
+            visited = set()
+            queue = [(root, 0)]
+            while len(queue) > 0:
+                node = queue.pop(0)
+                if queue == []:
+                    values.append(node[0].val)
+                elif queue[0][1] > node[1]:
+                    values.append(node[0].val)
+                if node[0].left is not None:
+                    queue.append((node[0].left, node[1] + 1))
+                if node[0].right is not None:
+                    queue.append((node[0].right, node[1] + 1))
+                
+        bfs(root)
+
+        return values
+                    
