2ef8a771b319b87ee9319a209c567c34df01f1db
Victor Verma <vpverm@bu.edu>
2024-06-30 00:55:06 -0400

    Time: 239 ms (5.16%) | Memory: 14.9 MB (5.27%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        water = 0
+        maxLeft = defaultdict(int)
+        maxRight = defaultdict(int)
+        maxLeft[0] = 0
+        maxRight[len(height)-1] = 0
+        for i in range(1, len(height)-1):
+            maxLeft[i] = max(maxLeft[i-1], height[i-1])
+        for i in range(len(height)-2, -1, -1):
+            maxRight[i] = max(maxRight[i+1], height[i+1])
+        for i in range(len(height)):
+            print(i, maxLeft[i], maxRight[i])
+            print(max(min(maxLeft[i], maxRight[i]) - height[i], 0))
+            water += max(min(maxLeft[i], maxRight[i]) - height[i], 0)
+        return water
+
+
+
+
+        
