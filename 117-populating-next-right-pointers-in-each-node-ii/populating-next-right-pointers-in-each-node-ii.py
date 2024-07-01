fa509680a2115cae9a2a7e48b596f88fe844c2b8
Victor Verma <vpverm@bu.edu>
2024-07-01 00:50:49 -0400

    Time: 38 ms (8.12%) | Memory: 13.8 MB (83.56%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root:
+            return root
+
+        def bfs(root):
+            queue = [(root, 0)]
+            while len(queue) > 0:
+                node, level = queue.pop(0)
+                if len(queue) == 0:
+                    node.next = None
+                else:
+                    if level == queue[0][1]:
+                        node.next = queue[0][0]
+                if node.left:
+                    queue.append((node.left, level + 1))
+                if node.right:
+                    queue.append((node.right, level + 1))
+        bfs(root)
+
+        return root
+
+        
