45831b511200e6774df634bb4d5e6ff1a75ce3dd
Victor Verma <vpverm@bu.edu>
2024-07-05 11:16:43 -0400

    Time: 787 ms (25.00%) | Memory: 21.1 MB (10.35%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -9,8 +9,13 @@ class Solution(object):
             maxRight[i] = max(maxRight[i+1], prices[i+1])
         
         largest = 0
+        lowest = float("inf")
         for i in range(len(prices)):
-            largest = max(largest, maxRight[i] - prices[i])
+            if prices[i] >= lowest:
+                continue
+            else:
+                lowest = prices[i]
+                largest = max(largest, maxRight[i] - prices[i])
         
         return largest
         
