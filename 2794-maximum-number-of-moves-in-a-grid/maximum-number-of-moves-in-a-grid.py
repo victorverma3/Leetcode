f77735917401c4d5935811dad31010b171ae2269
Victor Verma <vpverm@bu.edu>
2024-10-30 22:11:13 -0400

    Time: 159 ms (66.04%) | Memory: 29.8 MB (24.15%) - LeetSync

---
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def maxMoves(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+        optimal = [[None for col in range(len(grid[0]))] for j in range(len(grid))]
+
+        def search(row, col):
+
+            right, down, up = 0, 0, 0
+            if optimal[row][col]:
+                return 1 + optimal[row][col]
+
+            if col < len(grid[0]) - 1:
+                if grid[row][col+1] > grid[row][col]:
+                        right = search(row, col+1)
+                if row > 0:
+                    if grid[row-1][col+1] > grid[row][col]:
+                        down = search(row-1, col+1)
+                if row + 1 < len(grid):
+                    if grid[row+1][col+1] > grid[row][col]:
+                        up = search(row+1, col+1)
+
+            optimal[row][col] = max(right, down, up)
+            return 1 + optimal[row][col] 
+
+        maxMoves = 0
+        for i in range(len(grid)):
+            maxMoves = max(maxMoves, search(i, 0))
+        
+        return maxMoves - 1
+        
